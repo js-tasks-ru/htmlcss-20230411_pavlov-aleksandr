d1129228f03708b9ad7e4405b9daaa66eecc0df2
Alexon <mr.alexdemonp2003@mail.ru>
2023-07-12 14:20:06 +0300

    test_1

---
+++ assets/scripts/burger-menu.js
@@ -0,0 +1,21 @@
+const menu = document.getElementById('burger-menu');
+const menuClose = document.getElementById('burger-menu-close');
+const menuOpen = document.getElementById('burger-menu-open');
+
+if (menuOpen) {
+    menuOpen.addEventListener('click', function() {
+        if (menu) {
+            menu.setAttribute('data-open', '');
+            //menuOpen.style.display = "none";
+        }
+    });
+}
+
+if (menuClose) {
+    menuClose.addEventListener('click', function() {
+        if (menu) {
+            menu.removeAttribute('data-open');
+           // menuOpen.style.display = "block";  
+        }
+    });
+}
